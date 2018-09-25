2a2e3449602189c64b23fd97adbdee3114df13bd
sa6o sa6o <aleksandarboev95@gmail.com>
2018-09-25 21:20:36 +0300

    Removed some comments

--- LabsAndExercises/02.DataDefinitionAndDataTypes-Exercise.sql
+++ LabsAndExercises/02.DataDefinitionAndDataTypes-Exercise.sql
@@ -1,13 +1,8 @@
-#Preparations
+#Setup
 CREATE DATABASE `minions`;
 
 USE `minions`;
 
-/*
-In the newly created database Minions add table minions (id, name, age). Then add new table towns (id, name). Set
-id columns of both tables to be primary key as constraint. Submit your create table queries in Judge together for
-both tables (one after another separated by “;”) as Run queries &amp; check DB.
-*/
 #01. Create Tables 
 CREATE TABLE `minions` (
     `id` INT,
@@ -23,11 +18,6 @@ CREATE TABLE `towns` (
 );
 
 #02. Alter Minions Table 
-/*
-Change the structure of the Minions table to have new column town_id that would be of the same type as the id
-column of towns table. Add new constraint that makes town_id foreign key and references to id column of towns
-table. Submit your create table query in Judge as MySQL run skeleton, run queries &amp; check DB
-*/
 ALTER TABLE `minions`
 ADD COLUMN `town_id` INT;
 
@@ -54,22 +44,6 @@ DROP TABLE `minions`;
 DROP TABLE `towns`;
 
 #06. Create Table People 
-/*
-Using SQL query create table “people” with columns:
-
-© Software University Foundation. This work is licensed under the CC-BY-NC-SA license.
-Follow us: Page 1 of 5
- id – unique number for every person there will be no more than 2 31 -1people. (Auto incremented)
- name – full name of the person will be no more than 200 Unicode characters. (Not null)
- picture – image with size up to 2 MB. (Allow nulls)
- height – In meters. Real number precise up to 2 digits after floating point. (Allow nulls)
- weight – In kilograms. Real number precise up to 2 digits after floating point. (Allow nulls)
- gender – Possible states are m or f. (Not null)
- birthdate – (Not null)
- biography – detailed biography of the person it can contain max allowed Unicode characters. (Allow nulls)
-Make id primary key. Populate the table with 5 records. Submit your CREATE and INSERT statements in Judge as
-Run queries &amp; check DB.
-*/
 CREATE TABLE `people` (
     `id` INT UNSIGNED AUTO_INCREMENT,
     `name` VARCHAR(200) NOT NULL,
@@ -90,17 +64,6 @@ INSERT INTO `people`(`name`, `gender`, `birthdate`) VALUES
 ('Ivona', 'f', '1996-09-02');
 
 #07. Create Table Users 
-/*
-Using SQL query create table users with columns:
- id – unique number for every user. There will be no more than 2 63-1 users. (Auto incremented)
- username – unique identifier of the user will be no more than 30 characters (non Unicode). (Required)
- password – password will be no longer than 26 characters (non Unicode). (Required)
- profile_picture – image with size up to 900 KB.
- last_login_time
- is_deleted – shows if the user deleted his/her profile. Possible states are true or false.
-Make id primary key. Populate the table with 5 records. Submit your CREATE and INSERT statements. Submit your
-CREATE and INSERT statements as Run queries &amp; check DB.
-*/
 CREATE TABLE `users` (
     `id` BIGINT UNSIGNED AUTO_INCREMENT,
     `username` VARCHAR(30) UNIQUE NOT NULL,
@@ -119,11 +82,6 @@ INSERT INTO `users`(`username`, `password`, `last_login_time`, `is_deleted`) VAL
 ('The Magician', 'Aba kadabra', '18:49:00', 1);
 
 #08. Change Primary Key 
-/*
-Using SQL queries modify table users from the previous task. First remove current primary key then create new
-primary key that would be combination of fields id and username. The initial primary key name on id is pk_users.
-Submit your query in Judge as Run skeleton, run queries &amp; check DB.
-*/
 ALTER TABLE `users`
 MODIFY COLUMN `id` BIGINT UNSIGNED; #remove auto increment
 
@@ -141,11 +99,6 @@ ALTER TABLE `users`
 MODIFY COLUMN `last_login_time` TIMESTAMP DEFAULT NOW(); #"NOW()" does not work with datatype "TIME"
 
 #10. Set Unique Field 
-/*
-Using SQL queries modify table users. Remove username field from the primary key so only the field id would be
-primary key. Now add unique constraint to the username field. The initial primary key name on (id, username) is
-pk_users. Submit your query in Judge as Run skeleton, run queries &amp; check DB.
-*/
 ALTER TABLE `users`
 MODIFY COLUMN `id` BIGINT UNSIGNED;
 
@@ -164,20 +117,6 @@ MODIFY COLUMN `username` VARCHAR(30) UNIQUE;
 SELECT * FROM `users`;
 
 #11. Movies Database (91/100)
-/*
-Using SQL queries create Movies database with the following entities:
- directors (id, director_name, notes)
- genres (id, genre_name, notes)
- categories (id, category_name, notes)
- movies (id, title, director_id, copyright_year, length, genre_id, category_id, rating, notes)
-
-© Software University Foundation. This work is licensed under the CC-BY-NC-SA license.
-Follow us: Page 1 of 5
-Set most appropriate data types for each column. Set primary key to each table. Populate each table with 5
-records. Make sure the columns that are present in 2 tables would be of the same data type. Consider which fields
-are always required and which are optional. Submit your CREATE TABLE and INSERT statements as Run queries &amp;
-check DB.
-*/
 #Setup
 CREATE DATABASE `movies`;
 
@@ -254,22 +193,6 @@ INSERT INTO `movies`(`title`, `director_id`, `copyright_year`, `length`, `genre_
 ('Title5',  5, '2005', '01:30:00', 5, 5, 9.0);
 
 #12. Car Rental Database (75/100)
-/*
-Using SQL queries create car_rental database with the following entities:
- categories (id, category, daily_rate, weekly_rate, monthly_rate, weekend_rate)
- cars (id, plate_number, make, model, car_year, category_id, doors, picture, car_condition,
-available)
- employees (id, first_name, last_name, title, notes)
- customers (id, driver_licence_number, full_name, address, city, zip_code, notes)
- rental_orders (id, employee_id, customer_id, car_id, car_condition, tank_level,
-kilometrage_start, kilometrage_end, total_kilometrage, start_date, end_date,
-total_days, rate_applied, tax_rate, order_status, notes)
-Set most appropriate data types for each column. Set primary key to each table. Populate each table with 3
-records. Make sure the columns that are present in 2 tables would be of the same data type. Consider which fields
-are always required and which are optional. Submit your CREATE TABLE and INSERT statements as Run queries &amp;
-check DB.
-*/
-
 #Setup
 CREATE DATABASE `car_rental`;
 
@@ -372,15 +295,6 @@ INSERT INTO `rental_orders`
 (1, 1, 1, '2018-09-20', '2018-09-25', 'daily_rate', 20.0, 'Ongoing'),
 (2, 2, 2, '2018-09-21', '2018-09-26', 'monthly_rate', 20.0, 'About to begin'),
 (3, 3, 3, '2018-09-22', '2018-09-27', 'weekend_rate', 20.0, 'Finished');
-/*
-cars (id, plate_number, make, model, car_year, category_id, doors, picture, car_condition,
-available)
- employees (id, first_name, last_name, title, notes)
- customers (id, driver_licence_number, full_name, address, city, zip_code, notes)
- rental_orders (id, employee_id, customer_id, car_id, car_condition, tank_level,
-kilometrage_start, kilometrage_end, total_kilometrage, start_date, end_date,
-total_days, rate_applied, tax_rate, order_status, notes)
-*/
 
 #13. Hotel Database 
 #Setup
@@ -514,27 +428,6 @@ INSERT INTO `occupancies`(`employee_id`, `date_occupied`, `account_number`, `roo
 (2, '2018-09-26', 2, 2),
 (3, '2018-09-27', 3, 3);
 
-/*
- payments (id, employee_id, payment_date, account_number, first_date_occupied,
-last_date_occupied, total_days, amount_charged, tax_rate, tax_amount, payment_total,
-notes)
-Using SQL queries create Hotel database with the following entities:
- employees (id, first_name, last_name, title, notes)
- customers (account_number, first_name, last_name, phone_number, emergency_name,
-emergency_number, notes)
- room_status (room_status, notes)
- room_types (room_type, notes)
- bed_types (bed_type, notes)
- rooms (room_number, room_type, bed_type, rate, room_status, notes)
-
- occupancies (id, employee_id, date_occupied, account_number, room_number, rate_applied,
-phone_charge, notes)
-Set most appropriate data types for each column. Set primary key to each table. Populate each table with 3
-records. Make sure the columns that are present in 2 tables would be of the same data type. Consider which fields
-are always required and which are optional. Submit your CREATE TABLE and INSERT statements as Run queries &amp;
-check DB.
-*/
-
 #14. Create SoftUni Database 
 #Setup
 CREATE DATABASE `soft_uni`;
@@ -580,20 +473,6 @@ CREATE TABLE `employees` (
         REFERENCES `addresses` (`id`)
 );
 
-
-
-/*
-Now create bigger database called soft_uni. You will use database in the future tasks. It should hold information about
-•	towns (id, name)
-•	addresses (id, address_text, town_id)
-•	departments (id, name)
-•	employees (id, first_name, middle_name, last_name, job_title, department_id, hire_date, salary, address_id)
-Id columns are auto incremented starting from 1 and increased by 1 (1, 2, 3, 4…). 
-Make sure you use appropriate data types for each column. Add primary and foreign keys as constraints 
-for each table. Use only SQL queries. Consider which fields are always required and which are optional. 
-Submit your CREATE TABLE  statements as Run queries & check DB.
-*/
-
 #16. Basic Insert 
 INSERT INTO `towns` (`name`) VALUES ('Sofia'), ('Plovdiv'), ('Varna'), ('Burgas');
 INSERT INTO `departments` (`name`) 
@@ -604,15 +483,6 @@ INSERT INTO `employees` (`first_name`, `middle_name`, `last_name`, `job_title`, 
 ('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016-08-28',	525.25),
 ('Georgi', 'Terziev', 'Ivanov', 'CEO',  2,	'2007-12-09', 3000.00),
 ('Peter', 'Pan', 'Pan', 'Intern', 3, '2016-08-28',	599.88);
-
-/*
-Ivan Ivanov Ivanov	.NET Developer	Software Development	01/02/2013	3500.00
-Petar Petrov Petrov	Senior Engineer	Engineering	02/03/2004	4000.00
-Maria Petrova Ivanova	Intern	Quality Assurance	28/08/2016	525.25
-Georgi Terziev Ivanov	CEO	Sales	09/12/2007	3000.00
-Peter Pan Pan	Intern	Marketing	28/08/2016	599.88
-
-*/
 
 #17. Basic Select All Fields 
 SELECT * FROM `towns`;
