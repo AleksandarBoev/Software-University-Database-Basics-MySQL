39284d3cbf78e18e092c77ad48ad86cf25b3f500
sa6o sa6o <aleksandarboev95@gmail.com>
2018-10-01 16:58:59 +0300

    Added all tasks except the last one

--- LabsAndExercises/06.BuildInFunctions-Exercise/build_in_functions_exercise.sql
+++ LabsAndExercises/06.BuildInFunctions-Exercise/build_in_functions_exercise.sql
@@ -57,3 +57,52 @@ LIMIT 1000;
 #LIKE %a%a%a%
 #CHAR_LENGTH(REPLACE(`word`, 'a', '')) - CHAR_LENGTH(`word`) >= 3
 #LOCATE('a', `word`, LOCATE('a', `word`, LOCATE('a', `word`, 1))) = 1;
+#Setup
+USE `geography`;
+
+#Judge code
+SELECT `country_name`, `iso_code` FROM `countries`
+WHERE `country_name` LIKE '%a%a%a%'
+ORDER BY `iso_code` ASC
+LIMIT 1000;
+
+#11. Mix of Peak and River Names 
+SELECT  `peaks`.`peak_name`, `rivers`.`river_name`, 
+	LOWER(CONCAT(`peaks`.`peak_name`, SUBSTRING(`rivers`.`river_name`, 2))) AS 'mix'
+	FROM `rivers`, `peaks`
+WHERE RIGHT(`peaks`.`peak_name`, 1) = LEFT(`rivers`.`river_name`, 1)
+ORDER BY `mix`
+LIMIT 1000;
+
+#12. Games From 2011 and 2012 Year 
+#Setup
+USE `diablo`;
+
+#Judge code
+SELECT `name`, DATE_FORMAT(`start`, '%Y-%m-%d') AS 'start' FROM `games`
+WHERE EXTRACT(YEAR FROM `start`) IN (2011, 2012)
+ORDER BY `start` ASC, `name` ASC
+LIMIT 50;
+
+#13. User Email Providers 
+SELECT `user_name`, SUBSTRING(`email`, LOCATE('@', `email`) + 1) AS `Email Provider` FROM `users`
+ORDER BY `Email Provider` ASC, `user_name` ASC
+LIMIT 1000;
+
+#14. Get Users with IP Address Like Pattern 
+SELECT `user_name`, `ip_address` FROM `users`
+WHERE `ip_address` LIKE '___.1%.%.___'
+ORDER BY `user_name`
+LIMIT 1000;
+
+#15. Show All Games with Duration 
+SELECT `name`, 
+	IF(EXTRACT(HOUR FROM `start`) BETWEEN 0 AND 11, 'Morning', 
+		IF (EXTRACT(HOUR FROM `start`) BETWEEN 12 AND 17, 'Afternoon', 'Evening')) AS `Part of the Day`,
+	IF(`duration` <= 3, 'Extra Short', 
+		IF (`duration` BETWEEN 4 AND 6, 'Short', 
+			IF(`duration` BETWEEN 6 AND 10, 'Long', 'Extra Long'))) AS `Duration` 
+FROM `games`
+LIMIT 1000;
+
+#16. Orders Table 
